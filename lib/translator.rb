711aa28bf1f85dd79d9ba23b0b8bde81c6eb27b3
Vivian Tran <viviantran94@gmail.com>
2019-10-25 04:12:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,6 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   library = load_library(file_path)
   if library["get_emoticon"].include?(english_emoticon) 
     library["get_emoticon"][english_emoticon]
-  e
   end
 end
 
@@ -28,3 +27,4 @@ def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   if library["get_meaning"].include?(japanese_emoticon) 
     library["get_meaning"][japanese_emoticon]
   end
+end
