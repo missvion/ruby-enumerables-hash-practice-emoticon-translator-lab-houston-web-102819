9f03d65e5b884d4150d891421bd4aaece162bff3
Vivian Tran <viviantran94@gmail.com>
2019-10-25 04:10:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,21 +2,33 @@
 require 'yaml'
 require 'pry'
 
+def load_library
+  # code goes here
 def load_library(file_path)
-  library = YAML.load_file(lib/emoticons.yml)
-  emoticon_library = {"get_meaning" => {}, "get_emoticon" => {}}
+  library = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
   library.each do |meaning, emoticons|
-    emoticon_library["get_meaning"][emoticons[1]] = meaning
-    emoticon_library["get_emoticon"][emoticons[0]] = emoticons[1]
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
   end
- emoticon_library
+  result
 end
-
 
 def get_japanese_emoticon
   # code goes here
+#load_library('./lib/emoticons.yml')
+
+def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
+  library = load_library(file_path)
+  if library["get_emoticon"].include?(english_emoticon) 
+    library["get_emoticon"][english_emoticon]
+  end
 end
 
 def get_english_meaning
   # code goes here
+def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
+  library = load_library(file_path)
+  if library["get_meaning"].include?(japanese_emoticon) 
+    library["get_meaning"][japanese_emoticon]
 end
