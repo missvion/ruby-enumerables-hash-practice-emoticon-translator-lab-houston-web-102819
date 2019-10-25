75b1d67f4ad6dc91c87311f4c48675c25d854b83
Vivian Tran <viviantran94@gmail.com>
2019-10-25 03:48:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,10 @@
 # require modules here
+require "yaml"
 
 def load_library
-  # code goes here
+  YAML.load_file('emoticons.yml')
 end
+load_library
 
 def get_japanese_emoticon
   # code goes here
